0adfe003d7b6b7440e98826d355d292ca099e3d4
w5tf3 <48763345+w5tf3@users.noreply.github.com>
2023-07-06 14:43:47 +0200

    extend tests for time-axis construction

--- pyaceqd/tests/test_merge_interval.py
+++ pyaceqd/tests/test_merge_interval.py
@@ -25,4 +25,9 @@ print("expect:" + str([[-1,8]]))
 p1 = ChirpedPulse(tau_0=1,e_start=0, t0=1*4)
 p2 = ChirpedPulse(tau_0=1,e_start=0, t0=20)
 
-print(construct_t(0,30,0.1,1.0,p1,p2))
+print(construct_t(0,80,0.1,1.0,p1,p2,simple_exp=False))
+
+p1 = ChirpedPulse(tau_0=1,e_start=0, t0=1*4)
+p2 = ChirpedPulse(tau_0=1,e_start=0, t0=5)
+
+print(construct_t(0,80,0.1,1.0,p1,p2,simple_exp=True))
